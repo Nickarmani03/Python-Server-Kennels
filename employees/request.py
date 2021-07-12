f4e68931d7287f46a7616fd88ea0241c8e250f37
Nickarmani03 <nickarmani03@gmail.com>
2021-07-12 11:05:26 -0500

    first commit

---
+++ employees/request.py
@@ -0,0 +1,42 @@
+EMPLOYEES = [
+    {
+      "id": 1,
+      "name": "Jeremy Bakker",
+      "locationId": 1
+    },
+    {
+      "id": 2,
+      "name": "Cassius Clay",
+      "locationId": 1
+    },
+    {
+      "id": 3,
+      "name": "Travis Barker",
+      "locationId": 3
+    },
+    {
+      "id": 4,
+      "name": "Sean Combs",
+      "locationId": 2
+    },
+    {
+      "name": "Ron Swanson",
+      "location": "Nashville",
+      "locationId": 1,
+      "customerId": 3,
+      "id": 5
+    },
+    {
+      "name": "Homer Simpson",
+      "locationId": 3,
+      "id": 6
+    },
+    {
+      "name": "Clark Kent",
+      "locationId": 4,
+      "id": 7
+    }
+  ]
+
+def get_all_employees():
+    return EMPLOYEES
---
+++ locations/request.py
@@ -0,0 +1,25 @@
+LOCATIONS = [
+    {
+      "id": 1,
+      "name": "Nashville North",
+      "address": "8422 Johnson Pike"
+    },
+    {
+      "id": 2,
+      "name": "Nashville South",
+      "address": "209 Emory Drive"
+    },
+    {
+      "id": 3,
+      "name": "Lebanon Pike Kennels",
+      "address": "123 Wakanda St."
+    },
+    {
+      "name": "Murfreesboro Kennels",
+      "address": "123 Fake St.",
+      "id": 4
+    }
+  ]
+
+def get_all_locations():
+    return LOCATIONS
